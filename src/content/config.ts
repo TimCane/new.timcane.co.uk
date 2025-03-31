5eda7d7a973d7584f92dac716a51ab5c46e6afbf
Tim Cane <3074314+TimCane@users.noreply.github.com>
2025-03-31 19:38:04 +0000

    moved posts to projects, added more projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,19 @@ const postCollection = defineCollection({
   }),
 });
 
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    description: z.string(),
+    published: z.boolean(),
+    tags: z.array(z.string()).optional(),
+    featured: z.boolean().default(false),
+  }),
+});
+
 export const collections = {
   post: postCollection,
+  project: projectCollection,
 };
